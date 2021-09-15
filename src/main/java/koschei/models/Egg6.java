f8248b294fb942dc8e5796a11b4f0de8d760d3fd
Anton Garussky <www.snowfall@mail.ru>
2021-09-15 14:40:01 +0300

    refactor 2.1.4

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -1,12 +1,8 @@
 package koschei.models;
 
-import org.springframework.stereotype.Component;
-
-@Component
 public class Egg6 {
 
     private final Needle7 needle;
-
 
     public Egg6(Needle7 needle) {
         this.needle = needle;
